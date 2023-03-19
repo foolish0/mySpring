ec75142b7832553d02b6b8083ee21fa5cfc88988
Gabriel <lizhenjiang.xidian@foxmail.com>
2023-03-19 15:34:32 +0800

    Gabriel - 处理解析到的bean属性和构造器

--- src/main/java/com/myspring/beans/SimpleBeanFactory.java
+++ src/main/java/com/myspring/beans/SimpleBeanFactory.java
@@ -1,5 +1,8 @@
 package com.myspring.beans;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +37,90 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
             }
         }
         return singleton;
+    }
+
+    private Object createBean(BeanDefinition beanDefinition) {
+        Class<?> clz = null;
+        Object obj = null;
+        Constructor<?> con = null;
+
+        try {
+            clz = Class.forName(beanDefinition.getClassName());
+            // 处理构造器参数
+            ArgumentValues argumentValues = beanDefinition.getConstructorArgumentValues();
+            if (!argumentValues.isEmpty()) {
+                Class<?>[] paramTypes = new Class<?>[argumentValues.getArgumentCount()];
+                Object[] paramValues = new Object[argumentValues.getArgumentCount()];
+                for (int i = 0; i < argumentValues.getArgumentCount(); i++) {
+                    ArgumentValue argumentValue = argumentValues.getIndexedArgumentValue(i);
+                    if ("String".equals(argumentValue.getType()) || "java.lang.String".equals(argumentValue.getType())) {
+                        paramTypes[i] = String.class;
+                        paramValues[i] = argumentValue.getValue();
+                    } else if ("Integer".equals(argumentValue.getType()) || "java.lang.Integer".equals(argumentValue.getType())) {
+                        paramTypes[i] = Integer.class;
+                        paramValues[i] = Integer.valueOf((String) argumentValue.getValue());
+                    } else if ("int".equals(argumentValue.getType())) {
+                        paramTypes[i] = int.class;
+                        paramValues[i] = Integer.valueOf((String) argumentValue.getValue());
+                    } else {
+                        paramTypes[i] = String.class;
+                        paramValues[i] = argumentValue.getValue();
+                    }
+                }
+                try {
+                    // 按照特性构造器创建实例
+                    con = clz.getConstructor(paramTypes);
+                    obj = con.newInstance(paramValues);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                // 没有参数直接创建实例
+                obj = clz.newInstance();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // 处理属性
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        if (!propertyValues.isEmpty()) {
+            for (int i = 0; i < propertyValues.size(); i++) {
+                PropertyValue propertyValue = propertyValues.getPropertyValueList().get(i);
+                String pType = propertyValue.getType();
+                String pName = propertyValue.getName();
+                Object pValue = propertyValue.getValue();
+                Class<?>[] types = new Class<?>[1];
+                if ("String".equals(pType) || "java.lang.String".equals(pType)) {
+                    types[0] = String.class;
+                } else if ("Integer".equals(pType) || "java.lang.Integer".equals(pType)) {
+                    types[0] = Integer.class;
+                } else if ("int".equals(pType)) {
+                    types[0] = int.class;
+                } else {
+                    types[0] = String.class;
+                }
+
+                Object[] values = new Object[1];
+                values[0] = pValue;
+
+                // 按照setter的命名规范查找set方法，并调用
+                String methodName = "set" + pName.substring(0, 1).toUpperCase() + pName.substring(1);
+                Method method = null;
+                try {
+                    method = clz.getMethod(methodName);
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    method.invoke(clz, values);
+                } catch (InvocationTargetException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        return obj;
     }
 
     @Override
