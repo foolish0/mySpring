1fbcbcd4fc31c3382214b361d7d492a199d0e741
Gabriel <lizhenjiang.xidian@foxmail.com>
2023-03-20 23:14:49 +0800

    Gabriel - 注解bean的beanFactory

---
+++ src/main/java/com/myspring/beans/factory/support/AutowireCapableBeanFactory.java
@@ -0,0 +1,54 @@
+package com.myspring.beans.factory.support;
+
+import com.myspring.beans.BeansException;
+import com.myspring.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import com.myspring.beans.factory.config.BeanPostProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Gabriel
+ * @since 2023-03-20 22:49
+ */
+public class AutowireCapableBeanFactory extends AbstractBeanFactory{
+    private final List<AutowiredAnnotationBeanPostProcessor> beanPostProcessors = new ArrayList<>();
+
+    public void addBeanPostProcessor(AutowiredAnnotationBeanPostProcessor beanPostProcessor) {
+        this.beanPostProcessors.remove(beanPostProcessor);
+        this.beanPostProcessors.add(beanPostProcessor);
+    }
+
+    public int getBeanPostProcessorCount() {
+        return this.beanPostProcessors.size();
+    }
+
+    public List<AutowiredAnnotationBeanPostProcessor> getBeanPostProcessors() {
+        return this.beanPostProcessors;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorBeforeInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (AutowiredAnnotationBeanPostProcessor beanPostProcessor : getBeanPostProcessors()) {
+            beanPostProcessor.setBeanFactory(this);
+            result = beanPostProcessor.postProcessBeforeInitialization(result, beanName);
+            if (result == null) {
+                return result;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorAfterInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor beanPostProcessor : getBeanPostProcessors()) {
+            result = beanPostProcessor.postProcessAfterInitialization(result, beanName);
+            if (result == null) {
+                return result;
+            }
+        }
+        return result;
+    }
+}
