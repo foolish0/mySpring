9e6fa862120890f918b499a4ed49e0428025f106
Gabriel <lizhenjiang.xidian@foxmail.com>
2023-03-27 21:12:24 +0800

    Gabriel - 完善ApplicationContext抽象类

--- src/main/java/com/myspring/context/AbstractApplicationContext.java
+++ src/main/java/com/myspring/context/AbstractApplicationContext.java
@@ -2,11 +2,13 @@ package com.myspring.context;
 
 import com.myspring.beans.BeansException;
 import com.myspring.beans.factory.config.BeanFactoryPostProcessor;
+import com.myspring.beans.factory.config.BeanPostProcessor;
 import com.myspring.beans.factory.config.ConfigurableListableBeanFactory;
 import com.myspring.core.env.Environment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -24,6 +26,26 @@ public abstract class AbstractApplicationContext implements ApplicationContext {
     @Override
     public Object getBean(String beanName) throws BeansException {
         return getBeanFactory().getBean(beanName);
+    }
+
+    @Override
+    public Boolean containsBean(String name) {
+        return getBeanFactory().containsBean(name);
+    }
+
+    @Override
+    public boolean isSingleton(String name) {
+        return getBeanFactory().isSingleton(name);
+    }
+
+    @Override
+    public boolean isPrototype(String name) {
+        return getBeanFactory().isPrototype(name);
+    }
+
+    @Override
+    public Class<?> getType(String name) {
+        return getBeanFactory().getType(name);
     }
 
     public List<BeanFactoryPostProcessor> getBeanFactoryPostProcessors() {
@@ -66,6 +88,16 @@ public abstract class AbstractApplicationContext implements ApplicationContext {
     public abstract ConfigurableListableBeanFactory getBeanFactory() throws IllegalStateException;
 
     @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public Environment getEnvironment() {
+        return this.environment;
+    }
+
+    @Override
     public void addBeanFactoryPostProcessor(BeanFactoryPostProcessor postProcessor) {
         this.beanFactoryPostProcessors.add(postProcessor);
     }
@@ -78,5 +110,83 @@ public abstract class AbstractApplicationContext implements ApplicationContext {
     @Override
     public boolean isActive() {
         return true;
+    }
+
+    @Override
+    public boolean containsBeanDefinition(String beanName) {
+        return getBeanFactory().containsBeanDefinition(beanName);
+    }
+
+    @Override
+    public int getBeanDefinitionCount() {
+        return getBeanFactory().getBeanDefinitionCount();
+    }
+
+    @Override
+    public String[] getBeanDefinitionNames() {
+        return getBeanFactory().getBeanDefinitionNames();
+    }
+
+    @Override
+    public String[] getBeanNamesForType(Class<?> type) {
+        return getBeanFactory().getBeanNamesForType(type);
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        return getBeanFactory().getBeansOfType(type);
+    }
+
+    @Override
+    public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor) {
+        getBeanFactory().addBeanPostProcessor(beanPostProcessor);
+    }
+
+    @Override
+    public int getBeanPostProcessorCount() {
+        return getBeanFactory().getBeanPostProcessorCount();
+    }
+
+    @Override
+    public void registerDependentBean(String beanName, String dependBenaName) {
+        getBeanFactory().registerDependentBean(beanName, dependBenaName);
+    }
+
+    @Override
+    public String[] getDependentBeans(String beanName) {
+        return getBeanFactory().getDependentBeans(beanName);
+    }
+
+    @Override
+    public String[] getDependenciesForBean(String beanName) {
+        return getBeanFactory().getDependenciesForBean(beanName);
+    }
+
+    @Override
+    public void registerSingleton(String beanName, Object singletonObject) {
+        getBeanFactory().registerSingleton(beanName, singletonObject);
+    }
+
+    @Override
+    public Object getSingleton(String beanName) {
+        return getBeanFactory().getSingleton(beanName);
+    }
+
+    @Override
+    public boolean containsSingleton(String beanName) {
+        return getBeanFactory().containsSingleton(beanName);
+    }
+
+    @Override
+    public String[] getSingletonNames() {
+        return getBeanFactory().getSingletonNames();
+    }
+
+    public ApplicationEventPublisher getApplicationEventPublisher() {
+        return applicationEventPublisher;
+    }
+
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 }
